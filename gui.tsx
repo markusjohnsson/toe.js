14d887d3d8c2327a89bb76d5f2aeb4ad5e0e1ab8
Markus Johnsson <markus.johnsson@infviz.com>
2017-11-17 22:26:43 +0100

    Initial checkin

---
+++ gui.tsx
@@ -0,0 +1,82 @@
+
+import { BoardState, RowState, EmptyState, selectCell, GameState } from "./toe";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const CellStyle: React.CSSProperties = {
+    width: "100px",
+    height: "100px",
+    margin: "2px",
+    display: "inline-block",
+    verticalAlign: "middle",
+    backgroundColor: "silver"
+};
+
+const XCell = () => (
+    <svg style={{ width: "100px", height: "100px" }}>
+        <line x1="20" y1="20" x2="80" y2="80" stroke="gray" strokeWidth="20" />
+        <line x1="20" y1="80" x2="80" y2="20" stroke="gray" strokeWidth="20" />
+    </svg>);
+
+const OCell = () => (
+    <svg style={{ width: "100px", height: "100px" }}>
+        <circle cx="50" cy="50" r="30" stroke="gray" fill="transparent" strokeWidth="20"/>
+    </svg>);
+
+const RenderCell = (props: { cell: string, cellClicked: () => void }): JSX.Element => (
+    <div style={CellStyle} onClick={() => props.cellClicked()}>
+        {
+            props.cell == 'X' ? <XCell /> : 
+            props.cell == 'O' ? <OCell /> : ' '
+        }
+    </div>);
+
+const RenderRow = (props: { row: RowState, cellClicked: (column: number) => void }) => (
+    <div>
+        {
+            props.row.map((cell, c) => <RenderCell cellClicked={() => props.cellClicked(c)} key={c} cell={cell} />)
+        }
+    </div>);
+
+const RenderBoard = (props: { state: BoardState, cellClicked: (row: number, column: number) => void }) => (
+    <div>
+        {
+            props.state.map((row, r) => <RenderRow cellClicked={(column) => props.cellClicked(r, column)} key={r} row={row} />)
+        }
+    </div>);
+
+class GameBoard extends React.Component<{}, { gameState: GameState }> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { gameState: EmptyState }
+    }
+
+    onCellClicked = (r: number, c: number) => {
+        if (this.state.gameState.type == "settled")
+            return;
+
+        if (this.state.gameState.type == "tied")
+            return;
+
+        this.setState({ gameState: selectCell(this.state.gameState, r, c) });
+    }
+
+    render() {
+        return (
+            <div style={{ textAlign: "center", color: "gray", fontFamily: "sans-serif" }}>
+                {
+                    this.state.gameState.type == "settled" ?
+                        <h1>Vinnare: {this.state.gameState.winner}</h1> :
+                        this.state.gameState.type == "tied" ?
+                            <h1>Oavgjort</h1> :
+                            <h1>Nästa spelare: {this.state.gameState.turn}</h1>
+                }
+
+                <RenderBoard
+                    state={this.state.gameState.board}
+                    cellClicked={this.onCellClicked} />
+            </div>);
+    }
+}
+
+ReactDOM.render(<GameBoard />, document.querySelector("#root"));
---
+++ toe.ts
@@ -0,0 +1,79 @@
+
+export type Player = 'X' | 'O';
+export type SquareState = 'X' | 'O' | ' ';
+export type RowState = [SquareState,SquareState,SquareState];
+export type BoardState = [RowState,RowState,RowState];
+
+export type OngoingGame = { type: "ongoing", turn: Player, board: BoardState };
+export type SettledGame = { type: "settled", winner: Player, board: BoardState };
+export type TiedGame = { type: "tied", board: BoardState };
+export type GameState = OngoingGame | SettledGame | TiedGame;
+
+export const EmptyBoardState: BoardState = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+export const EmptyState: GameState = { type: "ongoing", turn: 'X', board: EmptyBoardState };
+
+export const stringify = (gameState: BoardState) =>
+    gameState.map(row => row.join(' ')).join('\n');
+
+const setRowState = (row: RowState, columnIndex: number, square: SquareState): RowState =>
+    [
+        columnIndex == 0 ? square : row[0],
+        columnIndex == 1 ? square : row[1],
+        columnIndex == 2 ? square : row[2]
+    ];
+
+const setSquareState = (board: BoardState, rowIndex: number, columnIndex: number, square: SquareState) : BoardState =>
+    [
+        rowIndex == 0 ? setRowState(board[0], columnIndex, square) : board[0],
+        rowIndex == 1 ? setRowState(board[1], columnIndex, square) : board[1],
+        rowIndex == 2 ? setRowState(board[2], columnIndex, square) : board[2]
+    ];
+
+const isRowSettled = (row: RowState) =>
+    row[0] != ' ' && row[0] == row[1] && row[1] == row[2];
+
+const isColumnSettled = (board: BoardState, columnIndex: number) =>
+    board[0][columnIndex] != ' ' && board[0][columnIndex] == board[1][columnIndex] && board[1][columnIndex] == board[2][columnIndex];
+
+const isDiagonalSettled = (board: BoardState) =>
+    (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+    (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) ;
+
+const isSettled = (board: BoardState) =>
+    isRowSettled(board[0]) || isRowSettled(board[1]) || isRowSettled(board[2]) ||
+    isColumnSettled(board, 0) || isColumnSettled(board, 1) || isColumnSettled(board, 2) ||
+    isDiagonalSettled(board);
+
+const isFreeCell = (c: SquareState) => c == ' ';
+    
+const hasFreeCells = (board: BoardState) => board.some(row => row.some(isFreeCell));
+
+export const selectCell = (gameState: OngoingGame, rowIndex: number, columnIndex: number): GameState => {
+    if (isFreeCell(gameState.board[rowIndex][columnIndex]) == false) {
+        return gameState;
+    }
+    const newBoard = setSquareState(gameState.board, rowIndex, columnIndex, gameState.turn);
+    const settled = isSettled(newBoard);
+    if (settled) {
+        return {
+            type: "settled",
+            winner: gameState.turn,
+            board: newBoard
+        };
+    }
+    else if (hasFreeCells(newBoard) == false) {
+        return {
+            type: "tied",
+            board: newBoard
+        };
+    }
+    else {
+        return {
+            type: "ongoing",
+            board: newBoard,
+            turn: gameState.turn == 'X' ? 'O' : 'X'
+        }
+    }
+}
+    
+
